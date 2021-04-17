17a3e80b50f5fa7bcfde648edcfb91c2fd82c13d
BarrothTheDragon <delia_bretan@yahoo.com>
2021-04-16 21:15:10 -0400

    optimized cover art storage to take less than 1GB, tests dont pass tho

--- MusicPlayer/MusicPlayer/MusicPlayerElements/Album.cs
+++ MusicPlayer/MusicPlayer/MusicPlayerElements/Album.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Windows.Media.Imaging;
 
 namespace MusicPlayer.MusicPlayerElements
@@ -11,7 +15,6 @@ namespace MusicPlayer.MusicPlayerElements
         public string[] Artists { get; set; }
         public string[] Genre { get; set; }
 
-        //TODO get cover art
         public byte[] Cover { get; set; }
 
         public Album(MusicFile musicFile, TagLib.File tagFile)
@@ -19,42 +22,68 @@ namespace MusicPlayer.MusicPlayerElements
             Title = musicFile.AlbumTitle;
             Artists = musicFile.AlbumArtist;
             Genre = musicFile.Genre;
-
-            if (tagFile.Tag.Pictures.Length >= 1)
-            {
-                Cover = (byte[])(tagFile.Tag.Pictures[0].Data.Data);
-                //Cover = LoadImage(CoverArt);
-            }
-            else
-            {
-                /*var pathToDefaultImage = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName, "Assets\\WelcomePotat.png");
-                Cover = new BitmapImage(new System.Uri(pathToDefaultImage));
-                Cover.Freeze();*/
-            }
-
             SongList = new List<MusicFile>();
+            Cover = (tagFile.Tag.Pictures.Length >= 1) ? CreateThumbnail(tagFile.Tag.Pictures[0].Data.Data, 200) : null;
         }
 
-        private static BitmapImage LoadImage(byte[] imageData)
+        public static byte[] CreateThumbnail(byte[] PassedImage, int LargestSide)
         {
-            if (imageData == null || imageData.Length == 0)
+            byte[] ReturnedThumbnail;
+
+            using (MemoryStream StartMemoryStream = new MemoryStream(),
+                                NewMemoryStream = new MemoryStream())
             {
-                return null;
+                // write the string to the stream  
+                StartMemoryStream.Write(PassedImage, 0, PassedImage.Length);
+
+                // create the start Bitmap from the MemoryStream that contains the image  
+                var startBitmap = new Bitmap(StartMemoryStream);
+
+                // set thumbnail height and width proportional to the original image.  
+                int newHeight;
+                int newWidth;
+                double HW_ratio;
+                if (startBitmap.Height > startBitmap.Width)
+                {
+                    newHeight = LargestSide;
+                    HW_ratio = (double)((double)LargestSide / (double)startBitmap.Height);
+                    newWidth = (int)(HW_ratio * (double)startBitmap.Width);
+                }
+                else
+                {
+                    newWidth = LargestSide;
+                    HW_ratio = (double)((double)LargestSide / (double)startBitmap.Width);
+                    newHeight = (int)(HW_ratio * (double)startBitmap.Height);
+                }
+
+                // create a new Bitmap with dimensions for the thumbnail.  
+                var newBitmap = new Bitmap(newWidth, newHeight);
+
+                // Copy the image from the START Bitmap into the NEW Bitmap.  
+                // This will create a thumnail size of the same image.  
+                newBitmap = ResizeImage(startBitmap, newWidth, newHeight);
+
+                // Save this image to the specified stream in the specified format.  
+                newBitmap.Save(NewMemoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                // Fill the byte[] for the thumbnail from the new MemoryStream.  
+                ReturnedThumbnail = NewMemoryStream.ToArray();
             }
 
-            var image = new BitmapImage();
-            using (var mem = new MemoryStream(imageData))
+            // return the resized image as a string of bytes.  
+            return ReturnedThumbnail;
+        }
+
+        // Resize a Bitmap  
+        private static Bitmap ResizeImage(Bitmap image, int width, int height)
+        {
+            var resizedImage = new Bitmap(width, height);
+            using (var gfx = Graphics.FromImage(resizedImage))
             {
-                mem.Position = 0;
-                image.BeginInit();
-                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.UriSource = null;
-                image.StreamSource = mem;
-                image.EndInit();
+                gfx.DrawImage(image, new Rectangle(0, 0, width, height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
             }
-            image.Freeze();
-            return image;
+            return resizedImage;
         }
     }
 }

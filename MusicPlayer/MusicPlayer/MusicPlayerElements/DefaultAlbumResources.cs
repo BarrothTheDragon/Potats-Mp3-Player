71b46e887af093501507b26fae524d9f54f3feba
BarrothTheDragon <delia_bretan@yahoo.com>
2021-04-16 21:29:33 -0400

    some cleanup, but final ahead

---
+++ MusicPlayer/MusicPlayer/MusicPlayerElements/DefaultAlbumResources.cs
@@ -0,0 +1,48 @@
+﻿
+namespace MusicPlayer.MusicPlayerElements
+{
+    class DefaultAlbumResources
+    {
+        private static readonly string DefaultAlbumTitle = "Potat's Album";
+        private static readonly string[] DefaultArtist = new string[] { "Potat" };
+        private static readonly string[] DefaultGenre = new string[] { "Potat's Music" };
+
+        internal static string GetValidAlbumTitle(string currentAlbumTitle)
+            => (currentAlbumTitle == null || currentAlbumTitle == "Unknown Album") ? DefaultAlbumTitle : currentAlbumTitle;
+
+        internal static string[] GetValidAlbumArtists(string[] albumArtists, string[] songArtist)
+        {
+            if (GetValidArtists(albumArtists) != DefaultArtist)
+            {
+                return albumArtists;
+            }
+            else if (GetValidArtists(songArtist) != DefaultArtist)
+            {
+                return songArtist;
+            }
+
+            return DefaultArtist;
+        }
+
+
+        internal static string[] GetValidArtists(string[] currentArtists)
+        {
+            if (currentArtists == null || currentArtists.Length == 0 || currentArtists[0] == "Unknown Artist")
+            {
+                return DefaultArtist;
+            }
+
+            return currentArtists;
+        }
+
+        internal static string[] GetValidGenre(string[] currentGenre)
+        {
+            if (currentGenre == null || currentGenre.Length == 0 || currentGenre[0] == "Unknown Genre")
+            {
+                return DefaultGenre;
+            }
+
+            return currentGenre;
+        }
+    }
+}
--- MusicPlayer/MusicPlayer/MusicPlayerElements/MusicFile.cs
+++ MusicPlayer/MusicPlayer/MusicPlayerElements/MusicFile.cs
@@ -4,10 +4,6 @@ namespace MusicPlayer.MusicPlayerElements
 {
     public class MusicFile
     {
-        private const string DefaultAlbumTitle = "Potat's Album";
-        private readonly string[] DefaultArtist = new string[] { "Potat" };
-        private readonly string[] DefaultGenre = new string[] { "Potat's Music" };
-
         public string Path { get; set; }
         public string AlbumTitle { get; set; }
         public string[] AlbumArtist { get; set; }
@@ -27,53 +23,15 @@ namespace MusicPlayer.MusicPlayerElements
         public MusicFile(TagLib.File musicFile)
         {
             Path = musicFile.Name;
-            AlbumTitle = GetValidAlbumTitle(musicFile.Tag.Album);
-            AlbumArtist = GetValidAlbumArtists(musicFile.Tag.AlbumArtists, musicFile.Tag.Performers);
+            AlbumTitle = DefaultAlbumResources.GetValidAlbumTitle(musicFile.Tag.Album);
+            AlbumArtist = DefaultAlbumResources.GetValidAlbumArtists(musicFile.Tag.AlbumArtists, musicFile.Tag.Performers);
             SongTitle = musicFile.Tag.Title ?? System.IO.Path.GetFileNameWithoutExtension(musicFile.Name);
-            SongArtist = GetValidArtists(musicFile.Tag.Performers);
-            Genre = GetValidGenre(musicFile.Tag.Genres);
+            SongArtist = DefaultAlbumResources.GetValidArtists(musicFile.Tag.Performers);
+            Genre = DefaultAlbumResources.GetValidGenre(musicFile.Tag.Genres);
             Year = (int)musicFile.Tag.Year;
             Disk = (int)musicFile.Tag.Disc;
             Track = (int)musicFile.Tag.Track;
             Duration = musicFile.Properties.Duration;
-        }
-
-        private string GetValidAlbumTitle(string currentAlbumTitle)
-            => (currentAlbumTitle == null || currentAlbumTitle == "Unknown Album") ? DefaultAlbumTitle : currentAlbumTitle;
-
-        private string[] GetValidAlbumArtists(string[] albumArtists, string[] songArtist)
-        {
-            if (GetValidArtists(albumArtists) != DefaultArtist)
-            {
-                return albumArtists;
-            }
-            else if (GetValidArtists(songArtist) != DefaultArtist)
-            {
-                return songArtist;
-            }
-
-            return DefaultArtist;
-        }
-
-
-        private string[] GetValidArtists(string[] currentArtists)
-        {
-            if (currentArtists == null || currentArtists.Length == 0 || currentArtists[0] == "Unknown Artist")
-            {
-                return DefaultArtist;
-            }
-
-            return currentArtists;
-        }
-
-        private string[] GetValidGenre(string[] currentGenre)
-        {
-            if (currentGenre == null || currentGenre.Length == 0 || currentGenre[0] == "Unknown Genre")
-            {
-                return DefaultGenre;
-            }
-
-            return currentGenre;
         }
     }
 }
